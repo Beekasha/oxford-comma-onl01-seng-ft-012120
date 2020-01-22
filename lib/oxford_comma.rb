4d9ea68bd5ca5d286a65540c2abe42cd8f1e4212
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-22 23:43:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,10 +9,10 @@ def oxford_comma(array)
     return array.join(" and ") #takes care of 2 elements
   else # more than 2 elements
     
-    new_array = array.map { |ele| ele + ", "}
-    new_array.pop
-    new_array << new_last_ele
-    new_array.join("")
+    new_array = array.map { |ele| ele + ", "} #creating new array with commas and spaces
+    new_array.pop #removes the last element, since it was incorrectly formatted
+    new_array << new_last_ele #replaces the last element with the correctly formatted element
+    new_array.join("") #converts array to a string
     
   end
 end
