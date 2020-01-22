22585802a7492d0c0c1f36da42d6b7eb8138ca43
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-22 23:32:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  last_ele = array[-1]
+  
+  if array.size < 2
+    return array.join("")
+  else
+    
+    new_array = array.pop
+    first_half_sentence = new_array.join(", ")
+    second_half_sentence = "and " #need to add last_ele
+    last_str = last_ele.to_s
+    full_sentence = first_half_sentence + second_half_sentence + last_str
+    return full_sentence
+    
+  end
+  
+    
+    
 
 end
